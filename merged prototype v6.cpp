bc6894d17b821bb529e837136f39637b9d3667b9
Daniel <daniel.tran01@sjsu.edu>
2024-11-22 10:44:30 -0800

    version 6 of the code, fixed the input thingy with cin.ignore

---
+++ merged prototype v6.cpp
@@ -0,0 +1,430 @@
+//============================================================================
+// Name        : merged.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <random>
+
+using namespace std;
+
+
+class Blackjack{
+public:
+
+void setup();
+void setmon(float);
+char hitloop();
+char wincond();
+int carddraw();
+string cardtype(int c);
+int cardtypev(int c);
+char inputval();
+char inputvalend();
+char endgame();
+
+
+private:
+int dcv;
+int pcv;
+int pic1;
+int pic2;
+vector<string>player;
+int d1;
+int d2;
+vector<string>dealer;
+float mon;
+float winnings;
+};
+
+
+
+
+//need card clear functoin
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//introduction
+	string name;
+		float money;
+		cout << "Welcome to the Hands On Casino where we would like to get hands on with you!!" << endl;
+		cout<<"What is your name dear customer?"<<endl;
+		cin>>name;
+		cout<<"Hello "<<name<<". How much money do you want to spend at our fine establishment($1 minimum bet):";
+		cin>>money;
+		while(money<1){
+			cout<<"$1 is required to make a bet. Please input the amount of money you want to spend:";
+			cin>>money;
+		}
+		cout<<"You entered "<<money<<endl;
+     Blackjack game;
+     game.setmon(money);
+     char gameloop1='t';    //change these values to char
+     char gameloop2='t';
+
+	 do{
+
+     game.setup();
+     do{
+
+     gameloop2=game.hitloop();
+
+     }while(gameloop2=='t');
+
+      game.wincond();
+
+	 }while(gameloop1=='t');
+
+	return 0;
+}
+
+
+
+char Blackjack::inputval(){
+
+	                                                                            //fyi bug isolation is around here somewhere
+	cout<<"do you want to hit?(type t for yes and f for no)"<<endl;
+		char in;
+		cin>>in;
+		std::cin.ignore(1000, '\n');
+		while(in!='t' && in!='f'){
+			cout<<"you didn't type t or f. If you want to hit, type t. If you do not, type f"<<endl;
+			cin>>in;
+		}
+	if(in=='t'){
+		return 't';
+	}
+	else{
+		return 'f';
+	}
+
+}
+
+char Blackjack::inputvalend(){
+
+	                                                                            //fyi bug isolation is around here somewhere
+	cout<<"do you want to play again?(type t for yes and f for no):"<<endl;
+		char in2;
+		std::cin.get(in2);
+		while(in2!='t' && in2!='f'){
+			cout<<"you didn't type t or f. If you want to hit, type t. If you do not, type f"<<endl;
+			cin>>in2;
+		}
+	if(in2=='t'){
+		return 't';
+	}
+	else{
+		return 'f';
+	}
+
+}
+
+char Blackjack::endgame(){
+				char end=inputvalend();
+				if(end=='t'){
+	             return 't';
+				}
+				else{
+				cout<<"exiting game";
+				exit(0);
+				return 'f';
+				//exit(0);
+				}
+}
+
+int Blackjack::carddraw(){
+
+	 vector<int>cd(3);
+
+
+	//deck of cards
+		random_device rng;
+
+			mt19937 gen(rng());
+
+			uniform_int_distribution<>deck(1,13);
+	         //cout<<"the elements of the vector are"<<endl;
+			//for(int i=0; i<cd.size(); i++){
+				//int random=dis(gen);
+			//	cd[i]=deck(gen);
+				//a[i]=random;
+
+				//cout<<cd[i]<<endl;
+		//	}
+
+	int card;
+
+	 //card=cd[cd.size()-1];
+
+     card=deck(gen);
+	//cout<<"the card drawn is"<<card<<endl;
+
+	return card;
+}
+
+
+
+string Blackjack::cardtype(int c){
+	switch(c){
+	case 1:
+		return "Ace";
+		break;
+	case 2:
+		return "2";
+		break;
+	case 3:
+		return "3";
+		break;
+	case 4:
+		return "4";
+		break;
+	case 5:
+		return "5";
+		break;
+	case 6:
+		return "6";
+		break;
+	case 7:
+		return "7";
+		break;
+	case 8:
+		return "8";
+		break;
+	case 9:
+		return "9";
+		break;
+	case 10:
+		return "10";
+		break;
+	case 11:
+		return "King";
+		break;
+
+	case 12:
+		return "Queen";
+		break;
+
+	case 13:
+		return "Jack";
+		break;
+
+	default:
+		return "no such card present";
+	}
+}
+
+
+int Blackjack::cardtypev(int c){
+
+
+	switch(c){
+	case 1:
+		return 1;
+		break;
+	case 2:
+		return 2;
+		break;
+	case 3:
+		return 3;
+		break;
+
+	case 4:
+		return 4;
+		break;
+
+	case 5:
+		return 5;
+		break;
+
+	case 6:
+		return 6;
+		break;
+
+	case 7:
+		return 7;
+		break;
+
+	case 8:
+		return 8;
+		break;
+
+	case 9:
+		return 9;
+		break;
+
+	case 10:
+		return 10;
+		break;
+
+	case 11:
+		return 10;
+		break;
+
+	case 12:
+		return 10;
+		break;
+
+	case 13:
+		return 10;
+		break;
+
+	}
+
+
+	return 0;
+}
+
+void Blackjack::setmon(float b){
+	mon=b;
+}
+
+void Blackjack::setup(){
+	 pic1=carddraw();
+
+	 pic2=carddraw();
+
+     pcv=0;
+     dcv=0;
+     winnings=0;
+
+
+
+	//cout<<"the first card is"<<pic1<<"the second one is"<<pic2;
+
+	cout<<"you have a "<<cardtype(pic1)<<" and a "<<cardtype(pic2)<<" in your hand."<<endl;
+
+	pcv+=cardtypev(pic1);
+
+	pcv+=cardtypev(pic2);
+
+	cout<<"the value in you hand is "<<pcv<<endl;
+
+	player.push_back(cardtype(pic1));
+
+	player.push_back(cardtype(pic2));
+
+	cout<<"the cards currently in your hand are"<<endl;
+	for(int i=0; i<player.size(); i++){
+		cout<<player[i]<<endl;
+	}
+
+	//dealer card intro:
+
+	 d1=carddraw();
+
+	 d2=carddraw();
+
+
+
+
+
+	cout<<"the dealer has pulled a "<<cardtype(d1)<<endl;
+
+	dcv+=cardtypev(d1);
+
+	dcv+=cardtypev(d2);
+
+	cout<<"the value in the dealer's hand is "<<dcv<<endl; //note will need to delete this for actual game
+
+	dealer.push_back(cardtype(d1));
+
+	dealer.push_back(cardtype(d2));
+
+	carddraw();
+
+	carddraw();
+
+	carddraw();
+
+}
+
+char Blackjack::hitloop(){
+	char in=inputval();
+	if(in=='t'){
+			int input=carddraw();
+			pcv+=cardtypev(input);
+
+			player.push_back(cardtype(input));
+
+			cout<<"the cards currently in your hand are"<<endl;
+				for(int i=0; i<player.size(); i++){
+					cout<<player[i]<<endl;
+				}
+				cout<<"the value in you hand is "<<pcv<<endl;
+
+			//use rule that dealer hits until they reach a combined card value of 17:
+				if(dcv<17){
+					int din=carddraw();
+					dcv+=cardtypev(din);
+					dealer.push_back(cardtype(din));
+					cout<<"the value in the dealer's hand is(before reach 17): "<<dcv<<endl;
+				}
+				cout<<"the value in the dealer's hand is "<<dcv<<endl;
+				return 't';
+		}
+	else{
+		cout<<"lets see what you won"<<endl;
+		return 'f';
+
+	}
+
+
+}
+
+
+
+char Blackjack::wincond(){
+
+	if(pcv>21){
+		cout<<"you just lost"<< mon*-1<< " dollars"<<endl;
+		winnings-=-mon;
+		cout<<"your current winnings are :"<<winnings<<endl;;
+				char out4=endgame();
+				return out4;
+
+	}
+
+
+	if((pcv<=21 && pcv>dcv)|| (pcv==21 && dcv>pcv)){    //won
+		cout<<"You just won:"<<mon*29<<" dollars"<<endl;
+		 winnings+=mon*29;
+
+		cout<<"your current winnings are :"<<winnings<<endl;;
+		char out1=endgame();
+       return out1;
+
+	}
+	//tie
+	if(pcv==dcv){
+		cout<<"tie"<<endl;
+		cout<<"your current winnings are :"<<winnings<<endl;
+		char out2=endgame();
+		 return out2;
+
+
+	}
+//lose
+	if((dcv<=21 && dcv>pcv)|| (dcv==21 && pcv>dcv)){          //this is not working
+		cout<< "you just lost"<< mon*-1<< " dollars"<<endl;
+		float lostlost=mon*-1;
+		winnings+=lostlost;
+		cout<<"your current winnings are :"<<winnings<<endl;
+		char out3=endgame();
+        return out3;
+
+
+	}
+	return 'f';
+}
